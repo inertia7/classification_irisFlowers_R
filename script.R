ea1efaf68615f4e6ed65e6b571c791cfab2bd494
raviolli77 <rauleulogio3@gmail.com>
2017-12-21 16:55:32 -0800

    restructure

--- script.R
+++ script.R
@@ -3,39 +3,45 @@
 
 # LOAD PACKAGES
 # Here if you haven't installed these packages do so!
-# install.packages("data.table")
 # install.packages("ggplot2")
 # install.packages("ggfortify")
 # install.packages("caret")
 # install.packages("class")
 # install.packages("gridExtra")
-# install.packages("scales")
 # install.packages("GGally")
 # install.packages("RGraphics")
-# install.packages("grid")
+# install.packages("plotly")
 # install.packages("gmodels")
 
-library(data.table)
 library(ggplot2)
 library(ggfortify)
 library(caret)
-library(class)
+library(e1071)
+library(here)
+library(tibble)
+#library(class)
 library(gridExtra)
-library(scales)
 library(GGally)
-library(RGraphics)
-library(grid)
+library(plotly)
 library(gmodels)
+
+here()
 
 # GET DATA
 attach(iris)
 data(iris)
-head(iris)
-
+iris_tb <- as_tibble(iris)
+head(iris_tb)
+colnames(iris_tb) <- c('sepal_length', 'sepal_width',
+                       'petal_length', 'petal_width', 
+                       'species')
 # EXPLORATORY ANALYSIS
 
 # SEPAL PROPERTIES INTERACTIONS
-gg1<-ggplot(iris,aes(x=Sepal.Width,y=Sepal.Length, shape=Species, color=Species)) + 
+gg1<-ggplot(iris_tb,
+            aes(x=sepal_width,y=sepal_length, 
+                shape=species, 
+                color=species)) + 
   theme(panel.background = element_rect(fill = "gray98"),
         axis.line   = element_line(colour="black"),
         axis.line.x = element_line(colour="gray"),
@@ -46,7 +52,10 @@ gg1<-ggplot(iris,aes(x=Sepal.Width,y=Sepal.Length, shape=Species, color=Species)
 ggplotly(gg1)
 
 # PETAL PROPERTIES INTERACTIONS
-gg2<-ggplot(iris,aes(x=Petal.Width,y=Petal.Length, shape=Species, color=Species)) + 
+gg2<-ggplot(iris_tb,
+            aes(x=petal_width,y=petal_length, 
+                shape=species, 
+                color=species)) + 
   theme(panel.background = element_rect(fill = "gray98"),
         axis.line   = element_line(colour="black"),
         axis.line.x = element_line(colour="gray"),
@@ -57,7 +66,9 @@ gg2<-ggplot(iris,aes(x=Petal.Width,y=Petal.Length, shape=Species, color=Species)
 ggplotly(gg2)
 
 # PAIRWISE MATRIX FOR OUR DATA SET
-pairs <- ggpairs(iris,mapping=aes(color=Species),columns=1:4) + 
+pairs <- ggpairs(iris_tb,
+                 mapping=aes(color=species),
+                 columns=1:4) + 
   theme(panel.background = element_rect(fill = "gray98"),
         axis.line   = element_line(colour="black"),
         axis.line.x = element_line(colour="gray"),
@@ -68,61 +79,44 @@ ggplotly(pairs) %>%
 
  # MODEL ESTIMATION
  # Creating training/test set 
-set.seed(123)
-samp.size <- floor(nrow(iris) * .75)
-samp.size
-set.seed(123)
+set.seed(88)
+trainIndex <- createDataPartition(iris_tb$species, 
+                                  p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+# Creating 80 20 split 
+training_set <- iris_tb[ trainIndex,]
 
-train.ind <- sample(seq_len(nrow(iris)), size = samp.size) 
-train.ind 
-train <- iris[train.ind, ] 
-head(train)
-
-train.set <- subset(train, select = -c(Species))
-head(train.set)
-
-test <- iris[-train.ind, ]
-head(test)
-
-test.set <- subset(test, select = -c(Species))
-head(test.set)
-
-class <- train[,"Species"]
-
-test.class <- test [,"Species"]
+test_set  <- iris_tb[-trainIndex,]
 
 # USING CARET PACKAGE TO ESTIMATE OPTIMAL K
-set.seed(123)
 
-contrl <- trainControl(method="repeatedcv",repeats = 3)
+fit <- train(species ~ ., 
+             data = training_set, 
+             method = "knn")
 
-knn.K <- train(Species ~ ., data = train, method = "knn", trControl = contrl, preProcess = c("center","scale"),tuneLength = 20)
-
-# Here we output the results from knn.K! 
-knn.K
+# Here we output the results from fit! 
+fit
 
 # PREDICTION RESULTS  
-knn.iris <- knn(train = train.set, test = test.set, cl = class, k = 9, prob = T)
+# Predict
+predict_test_set <- predict(fit,
+                            newdata = test_set)
 
-dim(train)
-dim(class)
-length(class)
-table(test.class, knn.iris)
-
-knn.iris
-
-CrossTable(x = test.class, y = knn.iris, prop.chisq=FALSE)
+CrossTable(x = test_set$species, 
+           y = predict_test_set, 
+           prop.chisq=FALSE)
  
 # WE CAN ESTIMATE OUR TEST ERROR RATE AS FOLLOWS FROM OUR TABLE:
-# > table(test.class, knn.iris)
-# knn.iris
-# test.class   setosa versicolor virginica
-# setosa         11          0         0
-# versicolor      0         13         0
-# virginica       0          1        13
+# > table(test_set$species, predict_test_set)
+# predict_test_set
+# setosa versicolor virginica
+# setosa         10          0         0
+# versicolor      0          8         2
+# virginica       0          0        10
 
-# Total Number of observations is 38 
-# Total Number of observations predicted incorrectly is 1
-1 / (11 + 13 + 13 + 1)
+# Total Number of observations is 30 
+# Total Number of observations predicted incorrectly is 2
 
-# WE RECEIVE A TEST ERROR RATE OF 0.02631579
+print(round(1 - fit$results$Accuracy[1], 4))
+# WE RECEIVE A TEST ERROR RATE OF 0.0334
